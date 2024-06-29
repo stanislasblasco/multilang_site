2a94c3f4207f2201db4bb506bedcad6aace0b9e1
stanblas83 <stanislas.blasco@ensea.fr>
2024-06-29 09:04:49 +0200

    deployement sur render

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from django.contrib import staticfiles
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rwb_bdb5fqlapwwn(z=x@#obsy3ir@u4xyla+ibna9pji@0#%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-
-ALLOWED_HOSTS = ['https://multilang-site-blrn.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,11 +74,11 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-import dj_database_url
-import os
+
 DATABASES = {
     'default': {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -132,11 +129,12 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', default="votre_cle_api_openai")
